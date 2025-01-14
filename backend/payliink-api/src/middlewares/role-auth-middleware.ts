367c310e6ec0febdd1ea26b636bf5f5b974f91e2
ntg <natatidioligirotto@gmail.com>
2025-01-14 00:53:50 -0300

    chore: retirado consolelog

--- backend/payliink-api/src/middlewares/role-auth-middleware.ts
+++ backend/payliink-api/src/middlewares/role-auth-middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 export const isAdmin = (req: Request, res: Response, next: NextFunction): void => {
   try {
-    console.log("Conteúdo de req.user:", req.user);
+  
 
     const user = req.user as { role: string };
 
